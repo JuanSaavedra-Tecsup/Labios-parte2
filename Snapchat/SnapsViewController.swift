771c0412dbc67427f6c6cebda50e35cab4d3354b
JuanSaavedra-Tecsup <juan.saavedra.p@tecsup.edu.pe>
2020-07-29 15:03:47 -0500

    lab15

---
+++ Snapchat/SnapsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SnapsViewController.swift
+//  Snapchat
+//
+//  Created by mbtec22 on 7/8/20.
+//  Copyright © 2020 TecsupDev. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad() 
+    }
+    
+    
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
